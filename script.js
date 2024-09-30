c6fe5767a113f855c8e5cc37e4b336ac4040822c
Utku Şen <144535374+utky35@users.noreply.github.com>
2024-09-30 22:09:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Dark/Light modu toggle
+const themeToggleButton = document.getElementById('theme-toggle');
+
+// Kullanıcı tercihlerini local storagedan alma
+const savedTheme = localStorage.getItem('theme');
+
+// Tema değiştir
+themeToggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  themeToggleButton.textContent = isDarkMode ? 'Light Mode' : 'Dark Mode';
+});
+
+// Scroll animasyon
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+  const triggerHeight = window.innerHeight / 1.2;
+  
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    
+    if (sectionTop < triggerHeight) {
+      section.classList.add('visible');
+    } else {
+      section.classList.remove('visible');
+    }
+  });
+});
