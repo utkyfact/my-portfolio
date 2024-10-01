7f09265a0bcc8efcd6bbb76b91be282815bb6870
Utku Sen <144535374+utkuusenn@users.noreply.github.com>
2024-10-01 13:31:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // Dark/Light modu toggle
 const themeToggleButton = document.getElementById('theme-toggle');
 
-// Kullanıcı tercihlerini local storagedan alma
-const savedTheme = localStorage.getItem('theme');
-
 // Tema değiştir
 themeToggleButton.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
